ead4d1ca2a4affe23c27da164c645af3d8cd0ac9
Devesh Kumar <devesh2027@gmail.com>
2024-09-01 12:18:56 +0530

    chore(Wrapper): Add /internal to logging API path

--- function-wrapper-server/utilities/constants.js
+++ function-wrapper-server/utilities/constants.js
@@ -1,2 +1,2 @@
 module.exports.VM_CONTROLLER_URL = "http://host.docker.internal:9163/api";
-module.exports.VM_LOG_SINK_URL = this.VM_CONTROLLER_URL + "/log";
+module.exports.VM_LOG_SINK_URL = this.VM_CONTROLLER_URL + "/internal/log";
